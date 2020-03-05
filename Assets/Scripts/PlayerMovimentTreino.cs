080fd6d9a8e8cd7890ef4cfa030e85fd0a1ec808
Lucianodasilvameyer <murcilhax@hotmail.com>
2020-03-05 00:22:39 -0300

    novas duvidas

--- Assets/Scripts/PlayerMovimentTreino.cs
+++ Assets/Scripts/PlayerMovimentTreino.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Mirror; //biblioteca necessaria para nomear uma classe de NetworkBehaviour?simm
 
-[RequireComponent(typeof(Rigidbody))] //para que serve?adiciona automaticamente
+[RequireComponent(typeof(Rigidbody))] //para que serve?adiciona automaticamente, mas igual eu criei a referencia do rigidbody?
 
 public class PlayerMovimentTreino : NetworkBehaviour // qual a diferença desta para a monobehaviour? serve para usar o islocalplayer
 {
@@ -14,14 +14,14 @@ public class PlayerMovimentTreino : NetworkBehaviour // qual a diferença desta 
 
     private void Awake()
     {
-        rigidbody_ref = GetComponent<Rigidbody>();
+        rigidbody_ref = GetComponent<Rigidbody>();//desnecessario por causa do [RequireComponent(typeof(Rigidbody))]?
     }
 
     private void Update()
     {
         if (isLocalPlayer)// serve para cada um controlar só seu personagem?sim
         {
-            float r = Input.GetAxis("Horizontal");
+            float r = Input.GetAxis("Horizontal"); //uma direção é um valor?
             float y = Input.GetAxis("Vertical");
             Movimento(r);
             Rotacao(y);
@@ -33,7 +33,7 @@ public class PlayerMovimentTreino : NetworkBehaviour // qual a diferença desta 
     }
     public void Movimento(float Input)
     {
-        float moviment = Input * velocidadeAndar * Time.deltaTime;
+        float moviment = Input * velocidadeAndar * Time.deltaTime; //os 3 podem ser unidos em um valor?
 
         Vector3 direction = new Vector3(moviment, 0, 0);  
 
